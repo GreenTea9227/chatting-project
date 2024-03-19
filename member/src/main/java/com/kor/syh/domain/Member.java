74cfb84680e9af7bdc19494a41198a41cbbd45fe
yohan <sjohn9227@naver.com>
2024-03-19 21:36:08 +0900

    feat: mysql 연결

--- member/src/main/java/com/kor/syh/domain/Member.java
+++ member/src/main/java/com/kor/syh/domain/Member.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Member {
 	private String id;
+	private String loginId;
 	private String password;
 	private String username;
 	private String nickname;
