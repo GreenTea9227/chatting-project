a687a6b9e38bbb357e21e8f66dc8937bc6f96ccb
yohan <sjohn9227@naver.com>
2024-03-19 21:23:29 +0900

    feat: member domain 생성

---
+++ member/src/main/java/com/kor/syh/domain/Member.java
@@ -0,0 +1,23 @@
+package com.kor.syh.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class Member {
+	private String id;
+	private String password;
+	private String username;
+	private String nickname;
+	private MemberStatus status;
+	private LocalDateTime createdDate;
+
+}
---
+++ member/src/main/java/com/kor/syh/domain/MemberStatus.java
@@ -0,0 +1,5 @@
+package com.kor.syh.domain;
+
+public enum MemberStatus {
+	USER,DELETE,BAN
+}
