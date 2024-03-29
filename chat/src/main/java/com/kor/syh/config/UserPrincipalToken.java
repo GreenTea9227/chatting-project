22e406b0afbf3390a187c2e8123b88d35cbef9e0
yohan <sjohn9227@naver.com>
2024-03-29 15:39:24 +0900

    feat: message 전달 시 custom principal을 이용하도록 조치
    1. principal에는 roomId 정보도 포함
    2. 예외 처리시 sendToUser를 이용하도록 조치

---
+++ chat/src/main/java/com/kor/syh/config/UserPrincipalToken.java
@@ -0,0 +1,24 @@
+package com.kor.syh.config;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserPrincipalToken extends UsernamePasswordAuthenticationToken {
+
+	@Setter
+	@Getter
+	private String roomId;
+
+	public UserPrincipalToken(Object principal, Object credentials,
+		Collection<? extends GrantedAuthority> authorities) {
+		super(principal, credentials, authorities);
+	}
+
+
+
+}
