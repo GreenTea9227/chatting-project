dbbbed1dcabd890617911acc8eb964ca4c7cb3aa
yohan <sjohn9227@naver.com>
2024-04-03 01:46:20 +0900

    feat: notification subscirbe mvctest

--- notification-service/src/main/java/com/kor/syh/notification/adpater/in/web/NotificationController.java
+++ notification-service/src/main/java/com/kor/syh/notification/adpater/in/web/NotificationController.java
@@ -1,9 +1,11 @@
 package com.kor.syh.notification.adpater.in.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -18,10 +20,10 @@ public class NotificationController {
 
 	private final NotificationUseCase notificationUseCase;
 
+	@ResponseStatus(value = HttpStatus.CREATED)
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public SseEmitter createSseEmitter(@RequestHeader (value = "X-Authorization-Id") String memberId) {
+	public SseEmitter createSseEmitter(@RequestHeader(value = "X-Authorization-Id") String memberId) {
 		return notificationUseCase.createNotificationChannel(memberId);
 	}
-
 
 }
