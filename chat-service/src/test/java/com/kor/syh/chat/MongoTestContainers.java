5a44281c4eca24c5b958c7a678c3aab87e165517
yohan <sjohn9227@naver.com>
2024-04-09 16:59:29 +0900

    feat: mongo testcontainer 사용시 나타나던 reached error 해결
    @Container를 사용하여서 자동으로 start,remove를 관리한 경우 발생한 에러로 수동으로 start 하도록 설정

--- chat-service/src/test/java/com/kor/syh/chat/MongoTestContainers.java
+++ chat-service/src/test/java/com/kor/syh/chat/MongoTestContainers.java
@@ -2,24 +2,13 @@ package com.kor.syh.chat;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
-public class MongoTestContainers {
+public abstract class MongoTestContainers {
 
-	@Container
-	private static final GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis"))
-		.withExposedPorts(6379);
-
-	@Container
-	private static final GenericContainer<?> mongoDBContainer = new GenericContainer(DockerImageName.parse("mongo"))
-		.withEnv("MONGO_INITDB_ROOT_USERNAME", "john")
-		.withEnv("MONGO_INITDB_ROOT_PASSWORD", "1111")
-		.withEnv("MONGO_INITDB_DATABASE", "chat")
-		.withExposedPorts(27017);
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
 
 	static {
 		mongoDBContainer.start();
@@ -27,15 +16,7 @@ public class MongoTestContainers {
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry registry) {
-		String mongoUri = String.format("mongodb://%s:%s@%s:%d/%s?authSource=admin",
-			"john", // MongoDB 사용자 이름
-			"1111", // MongoDB 비밀번호
-			mongoDBContainer.getHost(), // 동적으로 할당된 MongoDB 컨테이너의 호스트
-			mongoDBContainer.getFirstMappedPort(), // 동적으로 할당된 MongoDB 컨테이너의 포트
-			"chat" // 사용할 MongoDB 데이터베이스 이름
-		);
-
-		registry.add("spring.data.mongodb.uri", () -> mongoUri);
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
 }
--- chat-service/src/test/java/com/kor/syh/chat/RedisTestContainers.java
+++ chat-service/src/test/java/com/kor/syh/chat/RedisTestContainers.java
@@ -1,6 +1,5 @@
 package com.kor.syh.chat;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -8,17 +7,12 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
 @Testcontainers
 public class RedisTestContainers {
 
 	@Container
 	private static final GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis"))
 		.withExposedPorts(6379);
-
-	static {
-		redis.start();
-	}
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry registry) {
