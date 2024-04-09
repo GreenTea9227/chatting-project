9395507f6761672bab3c96d7e5f5fc34519ae4a2
yohan <sjohn9227@naver.com>
2024-04-09 22:51:52 +0900

    feat: api gateway global 예외 처리 추가

---
+++ gateway-service/src/main/java/com/kor/syh/filter/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.kor.syh.filter;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.kor.syh.common.CommonResponse;
+import com.kor.syh.common.jwt.TokenException;
+import com.kor.syh.common.utils.JsonUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Order(-1)
+@Component
+public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+	@Override
+	public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+
+		String message = ex.getMessage();
+
+		if (ex instanceof TokenException) {
+			exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+		} else {
+			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+		}
+
+		exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+		DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
+
+		String responseStr = JsonUtil.classToString(CommonResponse.fail(message));
+		DataBuffer dataBuffer = dataBufferFactory.wrap(responseStr.getBytes(StandardCharsets.UTF_8));
+
+		return exchange.getResponse().writeWith(Mono.just(dataBuffer));
+
+	}
+
+}
